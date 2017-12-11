10e0ab29300dfc7fb383fb008701ef35e01d3e8b
coliver <chrisgeorgeoliver@gmail.com>
2017-12-11 17:30:46 -0500

    Fix eslint in countType

--- countType.js
+++ countType.js
@@ -1,36 +1,29 @@
 // TODO: Make this room specific
-module.exports = function(type, qued) {
+module.exports = (type, qued) => {
   // console.log(`countType(${type}, ${qued})`);
-  if(qued == undefined)
-    qued = false;
+  if (qued === undefined) { qued = false; }
 
   // Find all creeps in that room by their role
-  var count = _.filter(Game.creeps, { memory: { role: type }}).length || 0;
+  let count = _.filter(Game.creeps, { memory: { role: type } }).length || 0;
   // console.log(`  count: ${count}`)
 
   // If qued was true, also check against the queue
-  if(qued)
-  {
-    var spawns = Game.spawns;
+  if (qued) {
+    const { spawns } = Game;
 
     // Check against currently spawning creeps
-    for(var i in spawns)
-    {
-      var spawn = spawns[i];
-      if(spawn.spawning !== null
+    Object.keys(spawns).forEach((spawn) => {
+      if (spawn.spawning !== null
         && spawn.spawning !== undefined
-        && Game.creeps[spawn.spawning.name].role == type) {
-        count++;
+        && Game.creeps[spawn.spawning.name].role === type) {
+        count += 1;
       }
-    }
+    });
 
-    if(Memory.spawnQue === undefined) { Memory.spawnQue = []; }
+    if (Memory.spawnQue === undefined) { Memory.spawnQue = []; }
 
     // Check the spawnQue itself
-    count += Memory.spawnQue.filter(function(qued)
-    {
-      return qued == type;
-    }).length;
+    count += Memory.spawnQue.filter(qued => qued === type).length;
   }
 
   return count;
