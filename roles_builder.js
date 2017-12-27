214a766b4ce5f95246302a6d75428f4915f16d00
coliver <coliver@users.noreply.github.com>
2017-12-27 13:43:44 -0500

    Massive builder rework

--- roles_builder.js
+++ roles_builder.js
@@ -17,145 +17,203 @@ module.exports = {
 
   action() {
     const { creep } = this;
-    // console.log(creep.name)
 
     // If out of energy, go to git sum and recharge
-    if (creep.carry.energy === 0) {
-      // STRUCTURE_CONTAINERs
-      // console.log("  checking containers...");
-      const containers = creep.room.find(FIND_STRUCTURES, {
-        filter: { structureType: STRUCTURE_CONTAINER },
-      });
-      // console.log(`    ${containers}`);
-      let target = null;
+    if (creep.carry.energy < creep.carryCapacity) {
+      this.findEnergy();
+      return true;
+    }
 
-      for (let i = 0; i < containers.length; i += 1) {
-        const container = containers[i];
-        // console.log(`  container: ${container}`);
+    return this.checkRamparts() ||
+           this.checkRepairs() ||
+           this.checkConstructionSites() ||
+           this.fixBrokenWalls() ||
+           this.rest(true);
+  },
 
-        if (container.isActive() && container.store[RESOURCE_ENERGY] > 0) {
-          target = container;
-          break;
-        }
-      }
+  closestThingWithEnergy() {
+    // console.log('      closestThingWithEnergy');
+    return this.closestContainerWithEnergy() ||
+           this.closestExtensionWithEnergy();
+  },
 
-      const closestSpawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
+  closestDroppedEnergy() {
+    const { creep } = this;
+    const energy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
 
-      if (closestSpawn) {
-        // console.log("  checking extensions...")
+    return energy;
+  },
 
-        const extensions = closestSpawn.room.find(FIND_MY_STRUCTURES, {
-          filter: { structureType: STRUCTURE_EXTENSION },
-        });
+  closestSourceWithEnergy() {
+    // console.log('      closestSourceWithEnergy');
+    return this.creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+  },
 
-        // console.log(`  ${spawn.name} has ${extensions.length} extensions available`);
+  closestContainerWithEnergy() {
+    // console.log('        closestContainerWithEnergy');
+    const container = this.creep.pos.findClosestByPath(FIND_STRUCTURES, {
+      filter: structure => structure.structureType === STRUCTURE_CONTAINER
+      && structure.isActive() && structure.store[RESOURCE_ENERGY] > 0,
+    });
+    // console.log(`          container: ${container}`);
+    return container;
+  },
 
-        for (let i = 0; i < extensions.length; i += 1) {
-          const ext = extensions[i];
-          if (ext.isActive() && (ext.energy > 0)) {
-            target = ext;
-            break;
-          }
-        }
-      }
+  closestExtensionWithEnergy() {
+    // console.log('        closestExtensionWithEnergy');
+    const extension = this.creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+      filter: structure => structure.structureType === STRUCTURE_EXTENSION && structure.energy > 0,
+    });
+    // console.log(`          extension: ${extension}`);
+    return extension;
+  },
 
-      if (target === null) {
-        // 250 minimum miner spawn cost
-        if (closestSpawn.energy > 250) {
-          target = closestSpawn;
-        } else {
-          // DO Nothing
-          this.rest(true);
-          return;
-        }
-      }
-
-      // console.log(`  target: ${target}`);
-
+  findEnergy() {
+    const { creep } = this;
+    const target = this.findATarget();
+    if (target) {
       if (creep.pos.isNearTo(target)) {
-        // console.log(`  getting nrg from ${closestSpawn.name}`)
         creep.withdraw(target, RESOURCE_ENERGY);
       } else {
-        // console.log(`  moving to ${closestSpawn.name}`);
         creep.moveTo(target, { visualizePathStyle: { stroke: this.myColor } });
       }
-    } else {
-      // First, we're going to check for damaged ramparts. We're using ramparts
-      // as the first line of defense and we want them nicely maintained. This
-      // is especially important when under attack. The builder will repair the
-      // most damaged ramparts first
-      const structures = creep.room.find(FIND_MY_STRUCTURES);
-      const damagedRamparts = [];
-
-      for (let index = 0; index < structures.length; index += 1) {
-        const structure = structures[index];
-        if (structure.structureType === 'rampart' && structure.hits < (structure.hitsMax - 50)) {
-          damagedRamparts.push(structure);
-        }
-      }
-
-      damagedRamparts.sort((a, b) => (a.hits - b.hits));
-
-      if (damagedRamparts.length) {
-        creep.moveTo(damagedRamparts[0], { visualizePathStyle: { stroke: this.myColor } });
-        creep.repair(damagedRamparts[0]);
-        return;
-      }
-
-      // Next we're going to look for general buildings that have less than 50%
-      // health, and we'll go to repair those. We set it at 50%, because we
-      // don't want builders abandoning their duty every time a road gets walked on
-      const halfBroken = creep.room.find(FIND_MY_STRUCTURES);
-      const toRepair = [];
-
-      for (let index = 0; index < halfBroken.length; index += 1) {
-        if ((halfBroken[index].hits / halfBroken[index].hitsMax) < 0.5) {
-          toRepair.push(halfBroken[index]);
-        }
-      }
-
-      if (toRepair.length) {
-        const structure = toRepair[0];
-        if (structure.pos.inRangeTo(creep.pos, 3)) {
-          creep.say('🛠️ repair');
-          creep.repair(structure);
-        } else {
-          creep.moveTo(structure, { visualizePathStyle: { stroke: this.myColor } });
-        }
-
-        return;
-      }
-
-      // If no repairs are needed, we're just going to go find some structures to build
-      const targets = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
-      if (targets) {
-        if (!creep.pos.isNearTo(targets)) {
-          creep.moveTo(targets, { visualizePathStyle: { stroke: this.myColor } });
-          return;
-        }
-        creep.say('⚒️ buildin');
-        creep.build(targets);
-        return;
-      }
-
-      // Nothing to build? fix some walls:
-      const repairit = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter(structure) {
-          return structure.structureType === STRUCTURE_WALL && structure.hits < 100000;
-        },
-      });
-
-      if (repairit) {
-        if (!creep.pos.isNearTo(repairit)) {
-          creep.moveTo(repairit, { visualizePathStyle: { stroke: this.myColor } });
-          return;
-        }
-        creep.say('🛠️ repair');
-        creep.repair(repairit);
-        return;
-      }
-
-      this.rest(true);
+      return;
     }
+
+    const energy = this.closestDroppedEnergy();
+    if (energy) {
+      if (creep.pos.isNearTo(energy)) {
+        // console.log(`  picking up ${energy}`);
+        creep.pickup(energy);
+        return;
+      }
+      creep.moveTo(energy, { visualizePathStyle: { stroke: this.myColor } });
+      return;
+    }
+
+    const source = this.closestSourceWithEnergy();
+    if (source != null) {
+      if (creep.pos.isNearTo(source)) {
+        creep.say('🔄 harvest');
+        creep.harvest(source);
+      } else {
+        creep.moveTo(source, { visualizePathStyle: { stroke: this.myColor } });
+      }
+    }
+  },
+
+  findATarget() {
+    let target = this.closestThingWithEnergy();
+
+    if (target) {
+      return target;
+    }
+
+    const closestSpawn = this.creep.pos.findClosestByRange(FIND_MY_SPAWNS);
+    if (closestSpawn.energy > 250) {
+      target = closestSpawn;
+    }
+
+    return target;
+  },
+
+  checkRamparts() {
+    const { creep } = this;
+    // First, we're going to check for damaged ramparts. We're using ramparts
+    // as the first line of defense and we want them nicely maintained. This
+    // is especially important when under attack. The builder will repair the
+    // most damaged ramparts first
+    const damagedRamparts = creep.room.find(FIND_MY_STRUCTURES, {
+      filter: structure => structure.structureType === STRUCTURE_RAMPART &&
+                           structure.hits < (structure.hitsMax - 50),
+    });
+
+    damagedRamparts.sort((a, b) => (a.hits - b.hits));
+
+    if (damagedRamparts.length) {
+      creep.moveTo(damagedRamparts[0], { visualizePathStyle: { stroke: this.myColor } });
+      creep.repair(damagedRamparts[0]);
+      return true;
+    }
+    return false;
+  },
+
+  checkRepairs() {
+    const { creep } = this;
+    // Next we're going to look for general buildings that have less than 50%
+    // health, and we'll go to repair those. We set it at 50%, because we
+    // don't want builders abandoning their duty every time a road gets walked on
+    const toRepair = creep.room.find(FIND_MY_STRUCTURES, {
+      filter: structure => (structure.hits / structure.hitsMax) < 0.5,
+    });
+
+    if (toRepair.length === 0) { return false; }
+
+    const structure = this.creep.pos.findClosestByPath(toRepair);
+
+    if (structure.pos.inRangeTo(creep.pos, 3)) {
+      creep.say('🛠️ repair');
+      creep.repair(structure);
+    } else {
+      creep.moveTo(structure, { visualizePathStyle: { stroke: this.myColor } });
+    }
+
+    return true;
+  },
+
+  checkConstructionSites() {
+    const { creep } = this;
+    // If no repairs are needed, we're just going to go find some structures to build
+    const target = this.findABuildSite();
+
+    if (target) {
+      if (!creep.pos.isNearTo(target)) {
+        creep.moveTo(target, { visualizePathStyle: { stroke: this.myColor } });
+        return true;
+      }
+      creep.say(`⚒️ ${target.structureType}`);
+      creep.build(target);
+      return true;
+    }
+    return false;
+  },
+
+  findABuildSite() {
+    return this.findContainerSites() ||
+      this.findExtensionSites();
+  },
+
+  findContainerSites() {
+    return this.findConstructionSites(STRUCTURE_CONTAINER);
+  },
+
+  findExtensionSites() {
+    return this.findConstructionSites(STRUCTURE_EXTENSION);
+  },
+
+  findConstructionSites(type) {
+    return this.creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES, {
+      filter: site => site.structureType === type,
+    });
+  },
+
+  fixBrokenWalls() {
+    const { creep } = this;
+    const repairit = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter(structure) {
+        return structure.structureType === STRUCTURE_WALL && structure.hits < this.WALL_REPAIR_MAX;
+      },
+    });
+
+    if (repairit) {
+      if (!creep.pos.isNearTo(repairit)) {
+        creep.moveTo(repairit, { visualizePathStyle: { stroke: this.myColor } });
+        return true;
+      }
+      creep.say('🛠️ repair');
+      creep.repair(repairit);
+      return true;
+    }
+    return false;
   },
 };
