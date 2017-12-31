6766a33c161af24b704b7205eb6b2ea803e50d9a
coliver <coliver@users.noreply.github.com>
2017-12-30 19:10:00 -0500

    Clean up transporter code

--- roles_transporter.js
+++ roles_transporter.js
@@ -1,105 +1,87 @@
 // TODO: Reconider this whole class. It's basically a unassigned miner_helper.
 const transporter = {
   parts: [
-    [CARRY, CARRY, MOVE, MOVE],
+    [CARRY, CARRY, WORK, MOVE],
   ],
 
   myColor: '#fa00ff',
 
+  closestContainerWithEnergy() {
+    console.log('    closestContainerWithEnergy');
+    const container = this.creep.pos.findClosestByPath(FIND_STRUCTURES, {
+      filter: structure => structure.structureType === STRUCTURE_CONTAINER
+      && structure.isActive() && structure.store[RESOURCE_ENERGY] > 0,
+    });
+    console.log(`      container: ${container}`);
+    return container;
+  },
+
+  closestDroppedEnergy() {
+    console.log('  closestDroppedEnergy');
+    const { creep } = this;
+    const energy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
+
+    return energy;
+  },
+
   action() {
     const { creep } = this;
-    // console.log(`${creep.name}`);
-
-    // @TODO: Balance Spawns here
+    console.log(`${creep.name}`);
 
     // TODO: Rework this to be a ammo hauler / extension filler
     // Check if there is anything in storage and if there are any low checkExtensions
     // and then fill them up.
     // Same with towers.
+    let target = null;
 
     if (_.sum(creep.carry) === 0) {
-      const closestSpawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS, {
-        filter(spawn) {
-          return spawn.energy > 250; // Minimum to spawn a miner
-        },
-      });
+      console.log('  find energy');
+      target = this.closestContainerWithEnergy() ||
+               this.closestDroppedEnergy();
 
-      // BEGIN FIND ENERGY
-      let target = null;
-
-      // STRUCTURE_CONTAINERs
       if (target === null) {
-        // console.log("  checking containers...");
-        const containers = creep.room.find(FIND_STRUCTURES, {
-          filter: { structureType: STRUCTURE_CONTAINER },
-        });
+        const source = this.creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
 
-        // console.log(`    ${containers}`);
-
-        for (let i = 0; i < containers.length; i += 1) {
-          const container = containers[i];
-          // console.log(`  container: ${container}`);
-          if (container.isActive() && container.store[RESOURCE_ENERGY] > 0) {
-            target = container;
-            break;
-          }
-        }
-      }
-
-      // If we didnt find any containers with nrg
-      // pull from spawn
-      if (target === null) {
-        if (creep.pos.isNearTo(closestSpawn)) {
-          // console.log(`  moving to ${closestSpawn} to get NRG`)
-          creep.withdraw(closestSpawn, RESOURCE_ENERGY);
+        if (creep.pos.isNearTo(source)) {
+          console.log(`  harvesting ${source}`);
+          creep.harvest(source);
         } else {
-          creep.moveTo(closestSpawn, { visualizePathStyle: { stroke: this.myColor } });
+          console.log(`  moving to ${source} to get NRG`);
+          creep.moveTo(source, { visualizePathStyle: { stroke: this.myColor } });
         }
-
-        return;
+      } else if (creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(target);
       }
-
       return;
     }
-    let target = null;
 
     // Transfer to builder
     if (!target) {
+      console.log('  find a builder');
       const builderToHelp = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
-        filter(builder) {
-          return (builder.memory.role === 'builder' || builder.memory.role === 'upgrader')
-              && _.sum(builder.carry) < (builder.carryCapacity / 2);
-        },
+        filter: builder => builder.memory.role === 'builder' && (_.sum(builder.carry) < (builder.carryCapacity)),
       });
       if (builderToHelp) {
+        console.log('    found a builder to help');
         target = builderToHelp;
       }
     }
 
-    // Alternatively, put it in an extension.
     if (!target) {
-      const extension = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
-        filter(structure) {
-          return structure.structureType === STRUCTURE_EXTENSION &&
-              structure.energy < structure.energyCapacity;
-        },
-      });
-
-      if (extension) {
-        target = extension;
-      }
+      console.log('  use a spawn');
+      target = this.creep.pos.findClosestByRange(FIND_MY_SPAWNS);
     }
 
     // Go to target and give it energy
     // TODO: CGO this wont work on extensions right now.
     if (creep.pos.isNearTo(target)) {
       if (_.sum(target.carry) < target.carryCapacity) {
-        // console.log(`  transferring ${creep.carry.energy} energy to ${target.name}`)
+        console.log(`  transferring ${creep.carry.energy} energy to ${target.name}`);
         creep.say('⚡ transferring');
         creep.transfer(target, RESOURCE_ENERGY);
       }
     } else {
-      // console.log(`  moving to ${target.name}`)
+      console.log(`  moving to ${target}`);
       creep.moveTo(target, { visualizePathStyle: { stroke: this.myColor } });
     }
   },
