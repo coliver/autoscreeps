12182344f06750ca01e4081632fbd707dd19cb88
coliver <coliver@users.noreply.github.com>
2017-12-31 18:26:22 -0500

    Fix transporters not using dropped energy

--- roles_transporter.js
+++ roles_transporter.js
@@ -17,10 +17,11 @@ const transporter = {
   },
 
   closestDroppedEnergy() {
-    // console.log('  closestDroppedEnergy');
+    // console.log('    closestDroppedEnergy');
     const { creep } = this;
     const energy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
 
+    // console.log(`      energy: ${energy}`);
     return energy;
   },
 
@@ -36,35 +37,44 @@ const transporter = {
 
     if (_.sum(creep.carry) === 0) {
       // console.log('  find energy');
-      target = this.closestContainerWithEnergy() ||
-               this.closestDroppedEnergy();
-
-      if (target === null) {
-        const source = this.creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
-
-        if (creep.pos.isNearTo(source)) {
-          // console.log(`  harvesting ${source}`);
-          creep.harvest(source);
-        } else {
-          // console.log(`  moving to ${source} to get NRG`);
-          creep.moveTo(source, { visualizePathStyle: { stroke: this.myColor } });
+      // First try containers
+      target = this.closestContainerWithEnergy();
+      if (target) {
+        if (creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
         }
-      } else if (creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(target);
+        return;
       }
+
+      // Then randomly dropped energy
+      target = this.closestDroppedEnergy();
+      if (target) {
+        if (creep.pickup(target) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
+        }
+        return;
+      }
+      // Other
+      const source = this.creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+      if (creep.pos.isNearTo(source)) {
+        // console.log(`  harvesting ${source}`);
+        creep.harvest(source);
+        return;
+      }
+
+      // console.log(`  moving to ${source} to get NRG`);
+      creep.moveTo(source, { visualizePathStyle: { stroke: this.myColor } });
       return;
     }
 
     // Transfer to builder
-    if (!target) {
-      // console.log('  find a builder');
-      const builderToHelp = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
-        filter: builder => builder.memory.role === 'builder' && (_.sum(builder.carry) < (builder.carryCapacity)),
-      });
-      if (builderToHelp) {
-        // console.log('    found a builder to help');
-        target = builderToHelp;
-      }
+    // console.log('  find a builder');
+    const builderToHelp = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+      filter: builder => (builder.memory.role === 'builder' || builder.memory.role === 'upgrader') && (_.sum(builder.carry) < (builder.carryCapacity)),
+    });
+    if (builderToHelp) {
+      // console.log('    found a builder to help');
+      target = builderToHelp;
     }
 
     if (!target) {
@@ -73,7 +83,6 @@ const transporter = {
     }
 
     // Go to target and give it energy
-    // TODO: CGO this wont work on extensions right now.
     if (creep.pos.isNearTo(target)) {
       if (_.sum(target.carry) < target.carryCapacity) {
         // console.log(`  transferring ${creep.carry.energy} energy to ${target.name}`);
